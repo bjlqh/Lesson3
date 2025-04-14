1fdfb040050a43ee737d1d13a1856f8f69485183
bjlqh <1499754436@qq.com>
2025-04-14 19:25:17 +0800

    创建通证合约

---
+++ FundToken.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+constract FundToken {
+    //1、通证的名字
+    //2、通证的简称
+    //3、通证的发行数量
+    //4、owner地址
+    //5、balance   address => uint256
+
+    //mint：获取通证 
+    /*
+        铸造一些通证到地址里面。 我现在一个通证都没有，我现在想拥有几个该怎么做?
+        我可以设置条件，让他转一些eth到我的合约里来。类似FundMe中 funderToAmount中，是否有值。
+    */
+
+    //transfer: transfer 通证
+    //balanceOf: 查看某一个地址通证的数量
+
+}
